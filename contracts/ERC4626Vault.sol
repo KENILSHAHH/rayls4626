1f3e55a2c141dccb651f48ec4996741b3644aa6d
kenilshahh <kenilshah1505@gmail.com>
2025-11-19 09:48:39 -0300

    initialize repo

---
+++ contracts/ERC4626Vault.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title ERC4626Vault
+ * @dev Example vault implementing ERC4626 standard
+ */
+contract ERC4626Vault is ERC4626 {
+    constructor(
+        ERC20 asset_,
+        string memory name_,
+        string memory symbol_
+    ) ERC4626(asset_) ERC20(name_, symbol_) {}
+}
